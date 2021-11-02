fc59b5b74ac1e073e992a0ca4f804c76bf69b443
jhj960918 <jhj960918@gmail.com>
2021-11-02 22:01:27 +0900

    Vue setting

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,62 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <div>
+      <v-app-bar   color="deep-purple accent-4" dense dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>SSAFY WEB</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
     </div>
-    <router-view/>
-  </div>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
 <script>
-import axios from 'axios';
-
-
 export default {
-  data() {
-    return {};
-  },
+  name: "App",
 
-  computed: {},
-  mounted() {
-    this.init();
-  },
-  methods : {
-    init() {
-      return axios
-          .get("/api/hello")
-          .then(res => {
-
-          console.log(res);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-    },
-
-  }
+  data: () => ({
+    //
+  }),
 };
 </script>
