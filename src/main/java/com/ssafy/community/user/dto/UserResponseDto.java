706a6760a0fb316a42b36e12f2f37ce8315f1e9c
jhj960918 <jhj960918@gmail.com>
2021-11-11 14:37:35 +0900

    bastTime Entity

--- src/main/java/com/ssafy/community/user/dto/UserResponseDto.java
+++ src/main/java/com/ssafy/community/user/dto/UserResponseDto.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -18,8 +19,9 @@ public class UserResponseDto {
     private String nickname;
     private String profileImage;
     private List<UserAuthority> authorities;
+    private LocalDateTime modifiedDate;
 
     public static UserResponseDto from(UserEntity user) {
-        return new UserResponseDto(user.getId(), user.getEmail(), user.getNickname(),user.getProfileImage(), user.getAuthorities());
+        return new UserResponseDto(user.getId(), user.getEmail(), user.getNickname(),user.getProfileImage(), user.getAuthorities(),user.getModifiedDate());
     }
 }
